383e05aa8b2677d80adf7774044e3607f5adb333
Tony <github email address>
2017-10-10 13:44:02 -0400

    fix bug on login with username that doesn't exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,12 +32,16 @@ class UsersController < ApplicationController
 
   #POST: /login
   post "/login" do
-    @user = User.find_by(username: params["username"])
-    if @user.authenticate(params["password"])
-      session["user_id"] = @user.id
-      redirect to "/beers"
+    if @user = User.find_by(username: params["username"])
+      if @user.authenticate(params["password"])
+        session["user_id"] = @user.id
+        redirect to "/beers"
+      else
+        flash[:message] = "The username and password you entered do not match our records."
+        redirect to "/login"
+      end
     else
-      flash[:message] = "The username and password you entered do not match our records."
+      flash[:message] = "That username cannot be found"
       redirect to "/login"
     end
   end
